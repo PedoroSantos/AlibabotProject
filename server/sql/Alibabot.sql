3eb6f828a30b6cc717740b2b3a3dff11ed3a2a55
MoraisGustavo1 <gmoraisildefonso@gmail.com>
2023-10-26 08:34:24 -0300

    changed sql server

---
+++ server/sql/Alibabot.sql
@@ -0,0 +1,34 @@
+create database Alibabot;
+use Alibabot;
+
+/* Create Database */
+create table Usuarios(
+idUsuario int primary key not null auto_increment,
+nomeUsuario varchar(50) not null,
+emailUsuario varchar(100) not null,
+senhaUsuario varchar(50) not null,
+termosCondicoes boolean not null
+);
+
+create table Funcionarios(
+idFuncionario int primary key not null auto_increment,
+nomeFuncionario varchar(70) not null,
+emailFuncionario varchar(100) not null,
+senhaFuncionario varchar(50) not null,
+areaFuncionario varchar(40) not null
+);
+
+create table APP(
+idAPP int primary key not null auto_increment,
+versaoAPP char(6) not null,
+tamanhoAPP char(5) not null
+);
+
+create table Chamados(
+idChamado  int primary key not null auto_increment,
+perguntaChamado varchar(256) not null,
+respostaChamado varchar(256) not null,
+dataEmissao date not null
+);
+
+show tables;
