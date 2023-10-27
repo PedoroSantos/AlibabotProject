87cf4dd81fabc9f02447e188198130a937301d66
caioempessoa <caiopolonioabruno@gmail.com>
2023-10-27 09:49:00 -0300

    changed $sql "usuarios" to "$cargo"

--- public/pages/lista-usuario.php
+++ public/pages/lista-usuario.php
@@ -1,16 +1,17 @@
 <?php
-include ("conexao.php");
-include ("banco-cliente.php");
+include ("../../server/conexao.php");
+include ("../../server/banco-dados.php");
 ?>
+
 <html>
 	<head>
 	<meta charset="UTF-8">
-	<title>Lista de Usuario</title>
+	<title>Lista de Funcionários</title>
 	</head>
 	<body>
-		<h1>Clientes Cadastrados</h1>
+		<h1>Funcionários Cadastrados</h1>
 		<h2>Menus</h2>
-		<a href="cadastro.php">Novo cadastro</a>
+		<a href="cadastro-usuarios.php">Novo cadastro</a>
 		<table>
 			<tr>
 				<td>Alterar</td>
@@ -18,22 +19,17 @@ include ("banco-cliente.php");
 				<td>Id</td>
 				<td>Nome</td>
 				<td>Email</td>
-				<td>Senha</td>
-				
 			</tr>
-			
 			<?php
-			$usuarios = listaBanco($conexao, "Usuario");
-			foreach ($usuarios as $usuario):
+			$usuarios = listaBanco($conexao, "usuarios");
+			foreach ($usuarios as $funcionario):
 			?>
-
 			<tr>
-				<td><a href="alterar.php?idClientes=<?php echo $usuario['idUsuarios']?>">Alterar</td>
-				<td><a href="excluir.php?idClientes=<?php echo $usuario['idUsuarios']?>">Excluir</td>
-				<td><?php echo $usuario['idUsuario']?></td>
-				<td><?php echo $usuario['nomeUsuario']?></td>
-				<td><?php echo $usuario['emailUsuario']?></td>
-				<td><?php echo $usuario['senhaUsuario']?></td>
+				<td><a href="alterar.php?idUsuario=<?php echo $funcionario['idUsuario']?>">Alterar</td>
+				<td><a href="excluir.php?idUsuario=<?php echo $funcionario['idUsuario']?>">Excluir</td>
+				<td><?php echo $funcionario['idUsuario']?></td>
+				<td><?php echo $funcionario['nome']?></td>
+				<td><?php echo $funcionario['email']?></td>
 			</tr>
 			<?php 
 			endforeach;
